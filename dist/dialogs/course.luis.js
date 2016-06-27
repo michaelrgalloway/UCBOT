e8e9d7b087d40885e4acc7f23be627e5d237594f
Ryan <michaelrgalloway@gmail.com>
2016-06-26 18:02:46 -0700

    variable scoping in dialog

--- dist/dialogs/course.luis.js
+++ dist/dialogs/course.luis.js
@@ -4,36 +4,35 @@ const globals_1 = require('../config/globals');
 const course_service_1 = require('../services/course/course.service');
 class CourseLuisDialog {
     constructor() {
-        this._courseService = new course_service_1.CourseService();
     }
     GetClassInformation(session, args) {
-        var cl = botframework.EntityRecognizer.findEntity(args.entities, 'course');
+        let cl = botframework.EntityRecognizer.findEntity(args.entities, 'course');
         //reach out to course service for some data
-        this._courseService = new course_service_1.CourseService();
-        var course = this._courseService.getCourseByCourseName(cl.entity);
-        session.send(`Here is some information for course **${cl.entity}**\r\n\r\n' +
-            '*School: ${course.school}\r\n\r\n' +
-            '*Building: ${course.building}\r\n\r\n' +
-            '*Instructor: ${course.instructor}\r\n\r\n' +
-            '*Required Books for Course: ISBN 14585421214, Book of Hard Knocks\r\n\r\n' +
-            '*Next available opening: ${course.nextsession}`);
+        let _courseService = new course_service_1.CourseService();
+        let course = _courseService.getCourseByCourseName(cl.entity);
+        session.send(`Here is some information for course **${cl.entity}**\r\n\r\n
+                      *School: ${course.school}\r\n\r\n
+                      *Building: ${course.building}\r\n\r\n
+                      *Instructor: ${course.instructor}\r\n\r\n
+                      *Required Books for Course: ISBN 14585421214, Book of Hard Knocks\r\n\r\n
+                      *Next available opening: ${course.nextsession}`);
     }
     GetBooks(session, args) {
-        var cl = botframework.EntityRecognizer.findEntity(args.entities, 'course');
-        session.send(`Here is the materials list for  **' + cl.entity + '** Summer 2016\r\n\r\n' +
-            '*ISBN 123456789 ![duck](http://aka.ms/Fo983c)\r\n\r\n' +
-            '*Tuesdays 8:30am - 9:30am\r\n\r\n' +
-            '*Thursdays 2:00pm - 3:30pm`);
+        let cl = botframework.EntityRecognizer.findEntity(args.entities, 'course');
+        session.send(`Here is the materials list for  **${cl.entity}** Summer 2016\r\n\r\n
+                      *ISBN 123456789 ![duck](http://aka.ms/Fo983c)\r\n\r\n
+                      *Tuesdays 8:30am - 9:30am\r\n\r\n
+                      *Thursdays 2:00pm - 3:30pm`);
     }
     GetClassSchedule(session, args) {
-        var cl = botframework.EntityRecognizer.findEntity(args.entities, 'course');
-        session.send(`Here is course schedule for  **' + cl.entity + '** Summer 2016\r\n\r\n' +
-            '*Mondays 8:00am - 9:30am\r\n\r\n' +
-            '*Tuesdays 8:30am - 9:30am\r\n\r\n' +
-            '*Thursdays 2:00pm - 3:30pm`);
+        let cl = botframework.EntityRecognizer.findEntity(args.entities, 'course');
+        session.send(`Here is course schedule for  **${cl.entity}** Summer 2016\r\n\r\n
+                      *Mondays 8:00am - 9:30am\r\n\r\n
+                      *Tuesdays 8:30am - 9:30am\r\n\r\n
+                      *Thursdays 2:00pm - 3:30pm`);
     }
     ConstructDialog() {
-        var dialog = new botframework.LuisDialog(globals_1.Globals.LUISDialogModelUrl);
+        let dialog = new botframework.LuisDialog(globals_1.Globals.LUISDialogModelUrl);
         dialog.on('GetBooks', this.GetBooks);
         dialog.on('GetClassInformation', this.GetClassInformation);
         dialog.on('GetClassSchedule', this.GetClassSchedule);
