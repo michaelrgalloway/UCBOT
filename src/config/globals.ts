2eb927e3eb827794613a27acd6b9a880002ee7f1
michaelrgalloway <michaelrgalloway@gmail.com>
2018-12-06 08:07:33 -0800

    Delete globals.ts

--- src/config/globals.ts
+++
@@ -1,7 +0,0 @@
-
-export class Globals {
-    public static LUISDialogModelUrl: string = "https://api.projectoxford.ai/luis/v1/application?id=e1c8189d-a1e1-460c-af14-c23f8719d69f&subscription-key=48fcce0fdf584caa8ccb0c93aa46b126";
-    public static botId:string = 'UCBOT';
-    public static botSecret:string = 'f0f5245b1b15439a850f051fb7c87133';
-
-}
